da36ff7ed2df9687784481dfff332948e0d847ee
DevChris38 <124736625+DevChris38@users.noreply.github.com>
2023-02-26 23:32:14 +0100

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,49 @@
+* {
+    box-sizing: border-box;
+}
+
+body {
+    background-color: rgb(188,0,0);
+    min-height: 100vh;
+    display: flex;
+}
+
+#central-bloc {
+    background-color: white;
+    margin: auto;
+    height: 200px;
+    width: 200px;
+    border-radius: 100px;
+    display: block;
+    position: relative;
+    text-decoration: none;
+}
+
+#g {
+    background-color: hsla(0, 0, 100, 0);
+    height: 100px;
+    width: 100px;
+    margin: 50px 50px;
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    text-decoration: none;
+    font-size: 50px;
+    color: black;
+    position: absolute;
+}
+
+#c {
+    background-color: hsla(0, 0, 100, 0);
+    height: 100px;
+    width: 100px;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    text-decoration: none;
+    font-size: 50px;
+    color: black;
+    position: absolute;
+    margin: 50px 50px;
+}
