17def5cf16c31e76658dc989d0ddf2ad26c08290
DevChris38 <124736625+DevChris38@users.noreply.github.com>
2023-02-26 23:42:23 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,49 +1,23 @@
-* {
-    box-sizing: border-box;
-}
+window.onload=function(){
+let bouton = document.getElementById('central-bloc');
+let g = document.getElementById('g');
+let c = document.getElementById('c');
 
-body {
-    background-color: rgb(188,0,0);
-    min-height: 100vh;
-    display: flex;
-}
-
-#central-bloc {
-    background-color: white;
-    margin: auto;
-    height: 200px;
-    width: 200px;
-    border-radius: 100px;
-    display: block;
-    position: relative;
-    text-decoration: none;
-}
-
-#g {
-    background-color: hsla(0, 0, 100, 0);
-    height: 100px;
-    width: 100px;
-    margin: 50px 50px;
-    display: flex;
-    justify-content: center;
-    padding: 20px;
-    text-decoration: none;
-    font-size: 50px;
-    color: black;
-    position: absolute;
-}
-
-#c {
-    background-color: hsla(0, 0, 100, 0);
-    height: 100px;
-    width: 100px;
-    margin: 0;
-    display: flex;
-    justify-content: center;
-    padding: 20px;
-    text-decoration: none;
-    font-size: 50px;
-    color: black;
-    position: absolute;
-    margin: 50px 50px;
+bouton.addEventListener("mouseover", function( event ) {
+  // on met l'accent sur la cible de mouseover
+  g.style.transform = "translate(-30px, -30px)";
+  g.style.transitionDuration = '1s';
+ 
+  c.style.transform = "translate(30px, 30px)";
+  c.style.transitionDuration = '1s';
+  
+}, false); 
+  
+  bouton.addEventListener("mouseleave", function( event ) {
+    g.style.transform = "";
+  g.style.transitionDuration = '1s';
+ 
+  c.style.transform = "";
+  c.style.transitionDuration = '1s';
+});
 }
